e022b7f50dd04f912e7eb8faddfa14d6fb6e77d0
Desenvolvedor SSG05 <joaomgsa@gmail.com>
2023-12-26 13:37:53 -0300

    incluindo transações

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,14 +10,21 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: MyHomePage()
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+   MyHomePage({super.key});
+
+  final _transactions =[
+      Transaction(
+        id: 't1', title: "Tenis de Corrida", value: 310.79, date: DateTime.now())
+        ,Transaction(
+        id: 't2', title: "Conta de luz", value: 210.30, date: DateTime.now())
+  ];
 
   @override
   Widget build(BuildContext context) {
