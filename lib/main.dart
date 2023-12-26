260559de4d8a6cd5ac5b6bdb8c6aaf9fc419eb78
Desenvolvedor SSG05 <joaomgsa@gmail.com>
2023-12-26 14:52:51 -0300

    estilização card

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,32 @@ class MyHomePage extends StatelessWidget {
       Column(
         children: _transactions.map((tr){
             return Card(
-              child: Text(tr.title),
+              child: Row(children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.purple.shade50,
+                      width: 2,) ),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    tr.value.toString(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  children:<Widget> [
+                    Text(tr.title),
+                    Text(tr.date.toString())
+                  ],
+                )
+              ]),
             );
         }).toList(),
       )
