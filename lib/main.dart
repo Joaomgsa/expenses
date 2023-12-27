0adaab03df0f0fc036fc3f2a06767cb18eedeb95
joaomgsa <joaomgsa@gmail.com>
2023-12-27 12:02:56 -0300

    criando formulário

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const ExpensesApp());
@@ -32,7 +33,6 @@ class MyHomePage extends StatelessWidget {
     ( appBar: AppBar(
       title: const Text('Despesas Pessoais'),
     ),body:  Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
       crossAxisAlignment: CrossAxisAlignment.stretch,
     children: <Widget>[
       const SizedBox(
@@ -52,11 +52,11 @@ class MyHomePage extends StatelessWidget {
                     vertical: 10),
                   decoration: BoxDecoration(
                     border: Border.all(
-                        color: Colors.purple.shade50,
+                        color: Colors.purple.shade500,
                       width: 2,) ),
                   padding: const EdgeInsets.all(10),
                   child: Text(
-                    tr.value.toString(),
+                    'R\$ ${tr.value.toStringAsFixed(2)}',
                     style: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
@@ -65,17 +65,54 @@ class MyHomePage extends StatelessWidget {
                   ),
                 ),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children:<Widget> [
-                    Text(tr.title),
-                    Text(tr.date.toString())
+                    Text(tr.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16
+                    ),),
+                    Text(
+                      DateFormat('d MMM y').format(tr.date)
+                      ,style: const TextStyle(
+                      color: Colors.grey
+                    ),)
                   ],
                 )
               ]),
             );
         }).toList(),
+      ),
+      const Card(
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Título'
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Valor (R\$)'
+                ),
+              ),
+              TextButton(
+                onPressed: minhaFuncao,
+                child: Text("Nova Transação"),
+               )
+            ],
+          ),
+        ),
       )
       ],
       ),
     );
   }
+}
+
+void minhaFuncao() {
+  print('botao pressionado');
 }
